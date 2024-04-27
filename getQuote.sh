6dbaf41048ef3b85a225f22d3dff23a90c5f439f
auslander <larsen.online@gmail.com>
2024-04-27 14:37:43 +0200

    add formatted quelltext

--- getQuote.sh
+++ getQuote.sh
@@ -2,5 +2,6 @@ LINES=`cat quelle.txt | wc -l`
 random_number=$((RANDOM % $LINES))
 
 random_number=$((random_number + 1))
-sed -n "${random_number}p" "quelle.txt" | sed 's|\/|\n|' > quotes.txt
+sed -n "${random_number}p" "quelle.txt" | sed 's|\/|\n|g' > quotes.txt
+#cat quotes.txt |wc -c
 node index.js
